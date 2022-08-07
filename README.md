6431aaff7161190f05edbbd637129e5e02421209
CaptainChrispy <cbrough000@gmail.com>
2022-08-07 10:13:50 -0600

    Creating and setting up base files for repository

---
+++ README.md
@@ -0,0 +1,43 @@
+# Sign-Up and Log-In Testing
+
+A simple respository of both manual and automated tests against the demoblaze website.
+
+    NOTE: You must have the latest version of Chrome (version 104.0.5112.79) 
+    in order for this program to run.
+
+# Requirements
+ - Python 3.10.0
+ -  Chrome version 104.0.5112.79
+ - The latest version of Selenium (use `pip install selenium` if not present)
+
+# Files
+## auto_login.py
+
+The main python script to run for the automated testing. 
+
+main() prepares the test environment by creating a Chrome web driver and reading into files to prepare login information before passing these to login() and passing returned values to log_result().
+
+login() takes in a string `username`, string `password`, and WebDriver `browser` and uses them to sign into the demoblaze website. It detects if an alert appears on the webpage before determining if the login failed or succeeded.
+
+log_result() takes in a boolean `result`, string `username`, and string `password` and writes login information to log.txt as well as to the console.
+
+## users.txt
+The file read for usernames and passwords. In each line, a username and password is separated by a comma with no additional spaces, like so:
+`user,password`
+This file can be altered to include more usernames and passwords. If this file is deleted, auto_login.py will throw an error and exit.
+
+## log.txt
+The file used to store information on login attempts. Every two lines stores basic information, with the first line dedicated to indicating the username and password tested and the second line indicating if the login succeeded or failed.
+
+## chromedriver.exe
+This driver is used by Selenium in order to open up a browser and automate the logins.
+
+## DemoBlaze Manual Testing.pdf
+A series of manual tests on the "Sign-Up" and Sign In flow. These are represented by two tables which specify what was tested and what the result was.
+
+# Automated Login Usage
+You can run the auto_login.py script by using `python auto_login.py` in a terminal or by running the script in your favorite Python IDE. **Ensure that all files in this repository are in the same directory before running.**
+
+A demonstration of running this program through the Windows command line is embedded below as a gif.
+
+![Gif of the use of the automated login script](auto_login.gif)
---
+++ auto_login.py
@@ -0,0 +1,107 @@
+###############################################################################
+# AUTOMATED TESTING
+# PURPOSE: To test successful logins and failed logins to the demo blaze website.
+# SITE: demoblaze.com/index.html
+# AUTHOR: Christopher Brough
+# DATE: 8/5/2022
+# NOTE: In order to run this, you MUST have the latest version of Chrome
+###############################################################################
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from os.path import exists
+from os import remove
+
+def log_result(result, username, password):
+    """Logs the result of a test."""
+    with open('log.txt', 'a') as log:
+        user_info = (f'Username: {username}, Password: {password}\n')
+        print(user_info.strip())
+        log.write(user_info)
+
+        if result:
+            outcome = '\tLogin successful\n'
+        else:
+            outcome = '\tLogin failed\n'
+
+        print(outcome.rstrip())
+        log.write(outcome)
+
+def login(username, password, browser):
+    """Logs into an account given a username and password."""
+    # Click the login navigation button
+    login_button = browser.find_element(By.ID,'login2')
+    login_button.click()
+
+    # Find and wait for login button
+    login_button = browser.find_element('xpath',"//button[@onclick='logIn()']")
+    WebDriverWait(browser,0.75).until(EC.element_to_be_clickable(login_button))
+
+    # Find the username and password.
+    username_field = browser.find_element(By.ID,'loginusername')
+    password_field = browser.find_element(By.ID,'loginpassword')
+
+    # Enter the username and password.
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+
+    # Click the login button
+    login_button.click()
+
+    # Check for alerts. If there are any, print "login failed", otherwise print "login successful".
+    try:
+        WebDriverWait(browser,0.57).until(EC.alert_is_present())
+        alert = browser.switch_to.alert
+        alert.accept()
+        browser.refresh()
+        return False
+    except:
+        logout_button = browser.find_element(By.ID,'logout2') 
+        logout_button.click()
+        return True
+
+def main():
+    """Prepare the test environment and run tests."""
+    # Open the browser and navigate to the website.
+    browser = webdriver.Chrome('chromedriver')
+    browser.get('https://demoblaze.com/index.html')
+
+    # For counting totals
+    fails = 0
+    successes = 0
+    
+    # If log exists, delete it
+    if exists('log.txt'):
+        remove('log.txt')
+
+    # Prepare usernames and passwords
+    users_and_pass = []
+    
+    if not exists('users.txt'):
+        print('users.txt does not exist. Exiting...')
+        return
+
+    with open('users.txt', 'r') as f:
+        users_pass = f.readlines()
+        for line in users_pass:
+            username, password = line.split(',')
+            users_and_pass.append((username, password.rstrip('\n')))
+
+    # Run the tests.
+    for username,password in users_and_pass:
+        result = login(username, password,browser)
+        log_result(result, username, password)
+        if result:
+            successes += 1
+        else:
+            fails += 1
+
+    # Quitting
+    print(f'Finished.\n\tSuccessful logins: {successes}\n\tFailed logins: {fails}\nQuitting...')
+    browser.quit()
+    return
+
+main()
